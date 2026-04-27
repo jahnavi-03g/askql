4602ded9688c59988e44d8b287a02cefc7262242
Jahnavi Gangishetti <gangishettijahnavi@gmail.com>
2026-04-26 20:43:04 -0400

    fix next config for amplify

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone", // optimised for Lambda/container deploys
 };
 
 module.exports = nextConfig;
